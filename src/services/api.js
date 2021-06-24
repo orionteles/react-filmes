5812ec90b9f70424848b88e61048889e6301e49b
Orion Teles de Mesquita <orionteles@gmail.com>
2021-06-23 23:23:31 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     headers: {
         'content-type':'application/json;charset=utf-8',
-        'Authorization':'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2NmQ3M2NjZmNmY2UwNzg0ODEwMGRmOWQ2OWQ4OTM4ZSIsInN1YiI6IjVhOTE1ZjM3OTI1MTQxNDE3ZDAwOTUzOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1LQBovGOn69k7BpSS0BUeJZ1sFAvrSPUz7ydm7ekKS0',
+        'Authorization':'',
     },    
 })
 
